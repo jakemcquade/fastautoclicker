790a2a7a7a551c724711269c597fa3d7927420fc
jakemcquade <183949683+jakemcquade@users.noreply.github.com>
2024-10-08 08:46:38 -0400

    Refacter Code For Types

--- src-tauri/src/inputs/minput.rs
+++ src-tauri/src/inputs/minput.rs
@@ -1,17 +1,16 @@
-use core_graphics::event::{ CGEvent, CGEventType, CGMouseButton };
-use core_graphics::event_source::{ CGEventSource, CGEventSourceStateID };
+use core_graphics::event::{CGEvent, CGEventType, CGMouseButton};
+use core_graphics::event_source::{CGEventSource, CGEventSourceStateID};
 use core_graphics::geometry::CGPoint;
-// use std::time::Duration;
-// use std::thread::sleep;
+use std::io;
 
-use crate::inputs::{ Action, Button };
+use crate::inputs::{Action, Button};
 
 pub trait Keylike: Copy {
-    fn produce_input(self, action: Action) -> CGEvent;
+    fn produce_input(self, action: Action) -> Result<CGEvent, io::Error>;
 }
 
 impl Keylike for Button {
-    fn produce_input(self, action: Action) -> CGEvent {
+    fn produce_input(self, action: Action) -> Result<CGEvent, io::Error> {
         let event_type = match (self, action) {
             (Button::Left, Action::Press) => CGEventType::LeftMouseDown,
             (Button::Left, Action::Release) => CGEventType::LeftMouseUp,
@@ -27,9 +26,12 @@ impl Keylike for Button {
             Button::Middle => CGMouseButton::Center,
         };
 
-        let point = get_current_mouse_location().ok_or(std::io::Error::new(std::io::ErrorKind::Other, "Failed to get mouse location"))?;
-        let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState);
-        CGEvent::new_mouse_event(source, event_type, point, button).unwrap()
+        let point = get_current_mouse_location().ok_or(io::Error::new(io::ErrorKind::Other, "Failed to get mouse location"))?;
+        let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState)
+            .ok_or(io::Error::new(io::ErrorKind::Other, "Failed to create CGEventSource"))?;
+        
+        CGEvent::new_mouse_event(source, event_type, point, button)
+            .ok_or(io::Error::new(io::ErrorKind::Other, "Failed to create mouse event"))
     }
 }
 
@@ -40,12 +42,14 @@ unsafe fn get_current_mouse_location() -> Option<CGPoint> {
 }
 
 #[inline(always)]
-pub fn send<K: Keylike>(key: K) {
-    let press_event = key.produce_input(Action::Press);
-    let release_event = key.produce_input(Action::Release);
-
-    let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState);
-    press_event.post(source)?;
-    // sleep(Duration::from_millis(1));
-    release_event.post(source)?;
+pub fn send<K: Keylike>(key: K) -> Result<(), io::Error> {
+    let press_event = key.produce_input(Action::Press)?;
+    let release_event = key.produce_input(Action::Release)?;
+    let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState)
+        .ok_or(io::Error::new(io::ErrorKind::Other, "Failed to create CGEventSource"))?;
+    
+    press_event.post(CGEventTapLocation::HID);
+    release_event.post(CGEventTapLocation::HID);
+    
+    Ok(())
 }
